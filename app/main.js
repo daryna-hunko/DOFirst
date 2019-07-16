8e96c596c04085186863dd6bc7fa38688ac7fb05
dhunko <18738469+dhunko@users.noreply.github.com>
2019-07-16 21:56:59 +0300

    adds cover page + header + footer from the react components

--- app/main.js
+++ app/main.js
@@ -1,42 +1,91 @@
-function formatDate(date) {
-return date.toLocaleDateString();
-}
-
-function Comment(props) {
-return (
-    <div className="Comment">
-    <div className="UserInfo">
-        <img
-        className="Avatar"
-        src={props.author.avatarUrl}
-        alt={props.author.name}
-        />
-        <div className="UserInfo-name">
-        {props.author.name}
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer>
+        <div className="container">
+          &copy; Слеплено Inc.
         </div>
-    </div>
-    <div className="Comment-text">{props.text}</div>
-    <div className="Comment-date">
-        {formatDate(props.date)}
-    </div>
-    </div>
+      </footer>
     );
+  }
 }
 
-const comment = {
-    date: new Date(),
-    text: 'I hope you enjoy learning React!',
-    author: {
-        name: 'Hello Kitty',
-        avatarUrl: 'https://placekitten.com/g/64/64',
-    },
-};
+class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <div className="container">
+          <div className="row">
+            <div className="col-9">
+              <h3 className="logo"><span>DO</span>First</h3>
+            </div>
+            <div className="col">
+              <div className="nav">
+                  <button type="button" className="btn btn-primary" data-toggle="modal" data-target="#login">
+                      Sign In
+                  </button>
+                  <button type="button" className="btn btn-primary" data-toggle="modal" data-target="#registrate">
+                      Sign Up
+                  </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+    );
+  }
+}
+
+class Main extends React.Component {
+  render() {
+    return (
+      <main>
+        <div className="cover-page">
+          <div className="container">
+            <div className="row">
+              <div className="col">
+                  <h1 className="logo"><span>DO</span>First</h1>
+                  <p className="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
+                  <p className="lead">
+                    <a href="#" className="btn btn-lg btn-secondary" data-toggle="modal" data-target="#registrate">Sign Up</a>
+                    <a href="#" className="btn btn-lg btn-secondary" data-toggle="modal" data-target="#login">Sign In</a>
+                  </p>
+              </div>
+              <div className="col">
+                  
+              </div>
+            </div>
+          </div>
+        </div>
+        
+      </main>
+    );
+  }
+}
+
+
+class Content extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Header/>
+        <Main/>
+        <Footer/>
+      </React.Fragment>
+    )
+  }
+}
+
+
+
+
+
+
+
+
+
+
 ReactDOM.render(
-<Comment
-    date={comment.date}
-    text={comment.text}
-    author={comment.author}
-/>,
+<Content/>,
 document.getElementById('root')
 );
-  
